72f268b082ad3c3ad0a7823a4aa956c4f91ef2a4
marcoaureliotomaz <marcoaureliotomaz@gmail.com>
2022-11-21 09:48:13 -0300

    Implementado o Get by Id de cozinhas no controller

--- src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
+++ src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -5,6 +5,7 @@ import com.algaworks.algafood.domain.repository.CozinhaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,5 +22,9 @@ public class CozinhaController {
     public List<Cozinha> listar(){
         return cozinhaRepository.listar();
 
+    }
+    @GetMapping("/{cozinhaId}")
+    public Cozinha buscar(@PathVariable Long cozinhaId){
+        return cozinhaRepository.buscar(cozinhaId);
     }
 }
