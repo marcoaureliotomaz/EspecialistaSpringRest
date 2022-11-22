e9affe846596982edd120f1a2c9a9609b5dc42e3
marcoaureliotomaz <marcoaureliotomaz@gmail.com>
2022-11-22 11:46:32 -0300

    Implementado DELETE de cozinha com tratamento inicial de error para recursos inexistentes e com erro de integridade relacional

--- src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
+++ src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -5,6 +5,7 @@ import com.algaworks.algafood.domain.model.Cozinha;
 import com.algaworks.algafood.domain.repository.CozinhaRepository;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -20,44 +21,60 @@ public class CozinhaController {
     private CozinhaRepository cozinhaRepository;
 
     @GetMapping()
-    public List<Cozinha> listar(){
+    public List<Cozinha> listar() {
         return cozinhaRepository.listar();
 
     }
+
     @GetMapping("/{cozinhaId}")
-    public ResponseEntity<Cozinha> buscar(@PathVariable Long cozinhaId){
+    public ResponseEntity<Cozinha> buscar(@PathVariable Long cozinhaId) {
         Cozinha cozinha = cozinhaRepository.buscar(cozinhaId);
-        if (cozinha != null){
+        if (cozinha != null) {
             return ResponseEntity.ok(cozinha);
         }
         return ResponseEntity.notFound().build();
     }
 
     @GetMapping(produces = MediaType.APPLICATION_XML_VALUE)
-    public CozinhasXMLWrapper listarXML(){
+    public CozinhasXMLWrapper listarXML() {
         return new CozinhasXMLWrapper(cozinhaRepository.listar());
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public void adicionar(@RequestBody Cozinha cozinha){
+    public void adicionar(@RequestBody Cozinha cozinha) {
         cozinhaRepository.salvar(cozinha);
     }
 
     @PutMapping("/{cozinhaId}")
     @ResponseBody
-    public ResponseEntity<Cozinha> atualizar(@PathVariable  Long cozinhaId, @RequestBody Cozinha cozinha){
+    public ResponseEntity<Cozinha> atualizar(@PathVariable Long cozinhaId, @RequestBody Cozinha cozinha) {
 
         Cozinha cozinhaAtual = cozinhaRepository.buscar(cozinhaId);
 
-        if (cozinhaAtual !=null) {
+        if (cozinhaAtual != null) {
             BeanUtils.copyProperties(cozinha, cozinhaAtual, "id");
             cozinhaRepository.salvar(cozinhaAtual);
             return ResponseEntity.ok(cozinhaAtual);
         }
 
         return ResponseEntity.notFound().build();
+    }
 
+    @DeleteMapping("/{cozinhaId}")
+    public ResponseEntity<Cozinha> remover(@PathVariable Long cozinhaId) {
+        Cozinha cozinha = cozinhaRepository.buscar(cozinhaId);
+        try {
+            if (cozinha != null) {
+                cozinhaRepository.remover(cozinha);
+
+                return ResponseEntity.noContent().build();
+            }
+        }catch(DataIntegrityViolationException e){
+            return ResponseEntity.status(HttpStatus.CONFLICT).build();
+        }
+
+        return ResponseEntity.notFound().build();
 
     }
 }
