7879cbf7943abe04dba32c935a2b6b66d31238c6
marcoaureliotomaz <marcoaureliotomaz@gmail.com>
2023-01-03 16:00:59 -0300

    Repositpory Cozinha usando JPQL no método consultar por nome

--- src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
+++ src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
@@ -96,7 +96,6 @@ public class RestauranteController {
         dadosOrigem.forEach((nomePropriedade, valorPropriedade) -> {
             Field field = ReflectionUtils.findField(Restaurante.class, nomePropriedade);
             field.setAccessible(true);
-
             Object novoValor = ReflectionUtils.getField(field, restauranteOrigem);
 
             ReflectionUtils.setField(field, restauranteDestino, novoValor);
