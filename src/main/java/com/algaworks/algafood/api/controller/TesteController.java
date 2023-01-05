bba6a81c97210088b7201ea4bbb2e2743871efff
marcoaureliotomaz <marcoaureliotomaz@gmail.com>
2023-01-05 11:59:12 -0300

    JPA com keywords para definir critgérios nas query methods

--- src/main/java/com/algaworks/algafood/api/controller/TesteController.java
+++ src/main/java/com/algaworks/algafood/api/controller/TesteController.java
@@ -1,9 +1,17 @@
 package com.algaworks.algafood.api.controller;
 
+import com.algaworks.algafood.domain.model.Cozinha;
+import com.algaworks.algafood.domain.model.Restaurante;
 import com.algaworks.algafood.domain.repository.CozinhaRepository;
+import com.algaworks.algafood.domain.repository.RestauranteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/teste")
@@ -12,8 +20,26 @@ public class TesteController {
     @Autowired
     private CozinhaRepository cozinhaRepository;
 
-  /*  @GetMapping("cozinhas/por-nome")
-    public List<Cozinha> cozinhasPorNome(@RequestParam("nome") String nome){
-            return cozinhaRepository.consultarPorNome(nome);
-    }*/
+    @Autowired
+    private RestauranteRepository restauranteRepository;
+
+    @GetMapping("cozinhas/por-nome")
+    public List<Cozinha> cozinhasPorNome( String nome){
+            return cozinhaRepository.findTodasByNomeContaining(nome);
+    }
+
+    @GetMapping("cozinhas/unico-por-nome")
+    public Optional <Cozinha> cozinhaPorNome(String nome){
+        return cozinhaRepository.findByNome(nome);
+    }
+
+    @GetMapping("restaurantes/por-taxa-frete")
+    public List <Restaurante> restaurantesPorTaxaFrete(BigDecimal taxaInicial, BigDecimal taxaFinal){
+        return restauranteRepository.findByTaxaFreteBetween(taxaInicial, taxaFinal);
+    }
+
+    @GetMapping("restaurantes/por-nome")
+    public List <Restaurante> restaurantesNomeCozinha(String nome, Long cozinhaId){
+        return restauranteRepository.findByNomeContainingAndCozinhaId(nome, cozinhaId);
+    }
 }
